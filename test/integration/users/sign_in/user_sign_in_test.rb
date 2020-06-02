5216dd5578e438d7968ef58e2719bf7286b0154b
blake <brobs0111@gmail.com>
2020-06-02 12:56:45 -0500

    testing if sign in error message works (not finished)

--- test/integration/users/sign_in/user_sign_in_test.rb
+++ test/integration/users/sign_in/user_sign_in_test.rb
@@ -1,16 +1,25 @@
 require 'test_helper'
-require 'minitest/autorun'
 
-class USER_SIGN_INTest < MiniTest::Unit::TestCase
-  def setup
-    # Do nothing
+class USER_SIGN_INTest < ActionDispatch::IntegrationTest
+  setup
+    @user = users :one
+    @event = events :one
+
+    sign_in
   end
 
-  def teardown
-    # Do nothing
+  test "error message displayed" do
+
+
   end
 
-  def test
-    skip 'Not implemented'
-  end
+def sign_in
+  visit root_path
+
+  click_on('Sign In', match: :first)
+
+  fill_in(id: 'user_email', with: 'fake@fake.com')
+  fill_in(id: 'user_password', with: 'user1234')
+
+  click_on(class: 'form-control btn-primary')
 end
